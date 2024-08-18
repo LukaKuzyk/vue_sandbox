68af6b39bed0a77f9debe668576980b63a078983
Luka Kuzyk <kuzykluka04@gmail.com>
2024-08-18 17:39:08 +0200

    composition api v2

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,57 @@
 // Composition api
-<script>
+<script setup>
 import {ref} from 'vue';
 
-export default {
-  setup(){
-    const name = ref('Luka Kuzyk');
-    const status = ref('pending');
-    const tasks = ref(['Task one', "Task two", 'Task three', "Task four"]);
+const name = ref('Luka Kuzyk');
+const status = ref('pending');
+const tasks = ref(['Task one', "Task two", 'Task three', "Task four"]);
+const newTasks = ref('');
 
-    const toggleStatus = () => {
-      if(status.value === 'active'){
-        status.value = 'pending';
-      }else if(status.value === 'pending'){
-        status.value = 'inactive';
-      }else {
-        status.value = 'active';
-      }
-    }
-
-    return{
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    }
+const toggleStatus = () => {
+  if (status.value === 'active') {
+    status.value = 'pending';
+  } else if (status.value === 'pending') {
+    status.value = 'inactive';
+  } else {
+    status.value = 'active';
   }
 }
+
+const addTask = () =>{
+  if(newTasks.value.trim() !== ''){
+    tasks.value.push(newTasks.value);
+    newTasks.value = '';
+  }
+};
+
+const deleteTask = (index) =>{
+  tasks.value.splice(index, 1);
+}
+
 </script>
 
 <template>
-  <h1>{{name}}</h1>
+  <h1>{{ name }}</h1>
   <p v-if="status === 'active'">User is active</p>
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTasks">
+    <button type="submit">Submit</button>
+  </form>
+
   <h3>Tasks:</h3>
   <ul>
-    <li v-for="task in tasks" :key="task">{{task}}</li>
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
+    </li>
   </ul>
 
 
-<!--  <button v-on:click="toggleStatus">Click</button>-->
-  <button @:click="toggleStatus">Click</button>
+  <!--  <button v-on:click="toggleStatus">Click</button>-->
+  <button @:click="toggleStatus">Click to change status</button>
 
 </template>
